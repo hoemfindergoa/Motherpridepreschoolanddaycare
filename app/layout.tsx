371678077dd445997d2abfa00d15b990402b67df
Ashish <90093173+ashurohilla@users.noreply.github.com>
2026-09-24 12:01:04 +0530

    ADD

--- app/layout.tsx
+++ app/layout.tsx
@@ -53,6 +53,20 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='' />
         <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet" />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-17825669915"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'AW-17825669915');
+            `,
+          }}
+        />
+
+
       </head>
       <body className='bg-[#fff9f5] text-slate-800'>
         {/* <Navbar/> */}
